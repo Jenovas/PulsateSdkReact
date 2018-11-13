511c0f72abf2fcfb3af77672c37ff62d690f6ab1
Rafael Skubisz <skubisz.rafael@gmail.com>
2018-11-13 17:53:21 +0100

    Release 1.0.11

--- Pulsate.js
+++ Pulsate.js
@@ -7,13 +7,13 @@ import { NativeModules, Platform } from 'react-native';
 const RNPulsateSdkReact = NativeModules.RNPulsateSdkReact;
 
 export const Pulsate = {
-    setAuthData: function (appid, appkey, gcmid) {
-        RNPulsateSdkReact.setAuthData(appid, appkey, gcmid);
+    setAuthorizationData: function (appid: string, appkey: string, gcmid: string) {
+        RNPulsateSdkReact.setAuthorizationData(appid, appkey, gcmid);
     },
-    startPulsateSession: function (successCallback, errorCallback) {
+    startPulsateSession: function (successCallback: () => string, errorCallback: () => string) {
         RNPulsateSdkReact.startPulsateSession(successCallback, errorCallback);
     },
-    startPulsateSessionForAlias: function (alias, successCallback, errorCallback) {
+    startPulsateSessionForAlias: function (alias: string, successCallback: () => string, errorCallback: () => string) {
         RNPulsateSdkReact.startPulsateSessionForAlias(alias, successCallback, errorCallback);
     },
 };

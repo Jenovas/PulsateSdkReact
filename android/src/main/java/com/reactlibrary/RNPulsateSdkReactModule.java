1f5192ce0e2d984d7d0f950f8d72b9d301717c5a
Rafael Skubisz <skubisz.rafael@gmail.com>
2018-12-11 11:04:05 +0100

    Release 1.0.34

--- android/src/main/java/com/reactlibrary/RNPulsateSdkReactModule.java
+++ android/src/main/java/com/reactlibrary/RNPulsateSdkReactModule.java
@@ -16,7 +16,6 @@ import java.util.List;
 
 public class RNPulsateSdkReactModule extends ReactContextBaseJavaModule {
     private final ReactApplicationContext reactContext;
-    private IPulsateManager pulsateManager;
 
     public RNPulsateSdkReactModule(ReactApplicationContext reactContext) {
         super(reactContext);
@@ -30,11 +29,12 @@ public class RNPulsateSdkReactModule extends ReactContextBaseJavaModule {
     
     @ReactMethod
     public void setAuthData(String appid, String appkey, String gcmid) {
-        pulsateManager = PulsateFactory.getInstance(new AuthorizationData(appid, appkey, gcmid));
+        PulsateFactory.getInstance(new AuthorizationData(appid, appkey, gcmid));
     }
 
     @ReactMethod
     public void startPulsateSession(Callback successCallback, Callback errorCallback) {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         final boolean[] wasCalled = {false};
         pulsateManager.startPulsateSession(new IPulsateRequestListener() {
             @Override
@@ -57,6 +57,7 @@ public class RNPulsateSdkReactModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void startPulsateSessionForAlias(String alias, Callback successCallback, Callback errorCallback) {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         final boolean[] wasCalled = {false};
         pulsateManager.startPulsateSessionForAlias(alias, new IPulsateRequestListener() {
             @Override
@@ -79,6 +80,7 @@ public class RNPulsateSdkReactModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void logoutCurrentAlias(Callback successCallback, Callback errorCallback) {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         final boolean[] wasCalled = {false};
         pulsateManager.logoutCurrentAlias(new IPulsateRequestListener() {
             @Override
@@ -101,36 +103,43 @@ public class RNPulsateSdkReactModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void setNewThreadButtonEnabled(boolean buttonEnabled) {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         pulsateManager.setNewThreadButtonEnabled(buttonEnabled);
     }
 
     @ReactMethod
     public void sendLocationWithBeaconEvents(boolean sendLocation) {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         pulsateManager.sendLocationWithBeaconEvents(sendLocation);
     }
     
     @ReactMethod
     public void setLocationUpdatesEnabled(boolean enabled) {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         pulsateManager.setLocationUpdatesEnabled(enabled);
     }
     
     @ReactMethod
     void setInAppNotificationEnabled(boolean enabled) {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         pulsateManager.setInAppNotificationEnabled(enabled);
     }
 
     @ReactMethod
     void showLastInAppNotification() {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         pulsateManager.showLastInAppNotification();
     }
 
     @ReactMethod
     void setPushNotificationEnabled(boolean enabled) {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         pulsateManager.setPushNotificationEnabled(enabled);
     }
 
     @ReactMethod
     void isPushNotificationEnabled(Callback successCallback, Callback errorCallback) {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         final boolean[] wasCalled = {false};
         if (pulsateManager.isPushNotificationEnabled()) {
             if (!wasCalled[0]) {
@@ -147,96 +156,115 @@ public class RNPulsateSdkReactModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void setUserAuthorized(boolean authorized) {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         pulsateManager.setUserAuthorized(authorized);
     }
 
     @ReactMethod
     public void showLastUnauthorizedMessage() {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         pulsateManager.showLastUnauthorizedMessage();
     }
 
     @ReactMethod
     public void updateFirstName(String firstName) {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         pulsateManager.updateFirstName(firstName);
     }
 
     @ReactMethod
     public void updateLastName(String lastName) {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         pulsateManager.updateLastName(lastName);
     }
 
     @ReactMethod
     public void updateEmail(String email) {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         pulsateManager.updateEmail(email);
     }
 
     @ReactMethod
     public void updateGender(int gender) {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         pulsateManager.updateGender(gender);
     }
 
     @ReactMethod
     public void updateAge(String age) {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         pulsateManager.updateAge(age);
     }
 
     @ReactMethod
     public void setPrivacy(int privacy) {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         pulsateManager.setPrivacy(privacy);
     }
 
     @ReactMethod
     public void createAttributeWithString(String attributeName, String value) {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         pulsateManager.createAttribute(attributeName, value);
     }
 
     @ReactMethod
     public void createAttributeWithFloat(String attributeName, float value) {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         pulsateManager.createAttribute(attributeName, value);
     }
 
     @ReactMethod
     public void createAttributeWithInt(String attributeName, int value) {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         pulsateManager.createAttribute(attributeName, value);
     }
 
     @ReactMethod
     public void createAttributeWithBool(String attributeName, boolean value) {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         pulsateManager.createAttribute(attributeName, value);
     }
 
     @ReactMethod
     public void createAttributeWithDate(String attributeName, Date value) {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         pulsateManager.createAttribute(attributeName, value);
     }
 
     @ReactMethod
     public void createEvent(String event) {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         pulsateManager.createEvent(event);
     }
 
     @ReactMethod
     public void incrementCounter(String counterName, int value) {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         pulsateManager.incrementCounter(counterName, value);
     }
 
     @ReactMethod
     public void decrementCounter(String counterName, int value) {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         pulsateManager.decrementCounter(counterName, value);
     }
 
     @ReactMethod
     public void forceAttributeSync() {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         pulsateManager.forceAttributeSync();
     }
 
     @ReactMethod
     public void showFeed() {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         pulsateManager.showFeed();
     }
 
     @ReactMethod
     public void setUserUnauthorizedListenerAndroid(Callback successCallback) {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         final boolean[] wasCalled = {false};
         pulsateManager.setUserUnauthorizedListener(new IPulsateUserUnauthorizedListener() {
             @Override
@@ -251,6 +279,7 @@ public class RNPulsateSdkReactModule extends ReactContextBaseJavaModule {
 
     @ReactMethod
     public void setUnreadCountUpdateListenerAndroid(Callback successCallback) {
+        IPulsateManager pulsateManager = PulsateFactory.getInstance();
         final boolean[] wasCalled = {false};
         pulsateManager.setUnreadCountUpdateListener(new IPulsateUnreadCountUpdateListener() {
             @Override
--- ios/RNPulsateSdkReact.podspec
+++ ios/RNPulsateSdkReact.podspec
@@ -1,7 +1,7 @@
 
 Pod::Spec.new do |s|
   s.name         = "RNPulsateSdkReact"
-  s.version      = "1.0.33"
+  s.version      = "1.0.34"
   s.summary      = "RNPulsateSdkReact"
   s.description  = <<-DESC
                   RNPulsateSdkReact
